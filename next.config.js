719ecf31a64be8f97716755d6c2f73509dbd36f1
Lioxcas <licastrox@gmail.com>
2023-01-18 15:03:23 -0300

    Project Dependencies Initial configuration and global styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+/*   output: { path: path.resolve(__dirname, "static") },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/[hash][ext]",
+      },
+    });
+    return config;
+  }, */
