e3934e2a1e497fafb77cfbf3109d1791e4882020
Lioxcas <licastrox@gmail.com>
2023-01-19 17:06:11 -0300

    Fix Cv Link - Added Resume in Spanish

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "../../../styles/Header.module.css";
-/* import CV from "../../../public/Lionel_Castro.pdf"; */
 
 const CTA = () => {
   return (
     <div className={styles.cta}>
-      <a href="" download className="btn">
+      <a
+        href="/CastroResume.pdf"
+        alt="alt text"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn"
+      >
         Download CV
       </a>
       <a href="#contact" className={`${"btn"} ${"btn-primary"}`}>
