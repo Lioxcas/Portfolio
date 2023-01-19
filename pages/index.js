85feb2b12f4b5aaa37dc5703be24bc3bbab184ad
Lioxcas <licastrox@gmail.com>
2023-01-19 16:17:31 -0300

    Change global styles

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Navbar from "../src/components/navbar/Navbar";
 import About from "../src/components/about/About";
 import Header from "../src/components/header/Header";
 import Skills from "../src/components/skills/Skills";
+import Portfolio from "../src/components/portfolio/Portfolio";
+import Contact from "../src/components/contact/Contact";
+import Footer from "../src/components/footer/Footer";
 
 export default function Home() {
   return (
@@ -17,6 +20,9 @@ export default function Home() {
       <Navbar />
       <About />
       <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 }
