ee35e748dcb1267cb7ede6ed98bc1ddd6678f000
Lioxcas <licastrox@gmail.com>
2023-01-19 16:15:00 -0300

    Footer Component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import styles from "../../../styles/Footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <a href="#" className={styles.footer__logo}>
+        Lionel Castro
+      </a>
+      <ul className={styles.permalinks}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Skills</a>
+        </li>
+        <li>
+          <a href="#porftolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className={styles.footer__socials}>
+        <a href="https://www.linkedin.com/in/lionel-castro/">
+          <BsLinkedin />
+        </a>
+        <a href="https://www.github.com/lioxcas/">
+          <BsGithub />
+        </a>
+      </div>
+      <div className={styles.footer__copyright}>
+        <small>&copy; Lionel Castro - Argentina 2023 - </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
